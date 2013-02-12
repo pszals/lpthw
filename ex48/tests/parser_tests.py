01277dfd6784e9112bf64f4e003a34d1db79f20c
Philip Szalwinski <pszalwinski@gmail.com>
2013-02-11 20:54:38 -0500

    test_parse_verb() is up and working

--- ex48/tests/parser_tests.py
+++ ex48/tests/parser_tests.py
@@ -4,12 +4,20 @@ from bin import parser
 word_list = [('alpha', 'letter_a'), ('charlie', 'letter_c')]
 
 def test_peek():
+	word_list = [('alpha', 'letter_a'), ('charlie', 'letter_c')]
 	assert_equal(parser.peek(word_list), 'alpha')
 	result = parser.peek(word_list)
 	assert_equal(result, 'alpha')
 	
 def test_match():
+	word_list = [('alpha', 'letter_a'), ('charlie', 'letter_c')]
 	assert_equal(parser.match(word_list, 'alpha'), ('alpha', 'letter_a'))
 	
 def test_skip():
+	word_list = [('alpha', 'letter_a'), ('charlie', 'letter_c')]
 	assert_equal(parser.skip(word_list, 'alpha'), None)
+	
+def test_parse_verb():
+	word_list = [('verb', 'go'), ('stop', 'the')]
+	assert_equal(parser.parse_verb(word_list), ('verb', 'go'))
+
