7c56b2f2c34fb7025292839a8a3d6115f7c5b8b1
Philip Szalwinski <pszalwinski@gmail.com>
2013-02-11 20:30:23 -0500

    Fixed test_peek(): so that it would test a tuple instead of just item in a list

--- ex48/tests/parser_tests.py
+++ ex48/tests/parser_tests.py
@@ -1,12 +1,12 @@
 from nose.tools import *
 from bin import parser
 
-word_list = ['alpha', 'bravo', 'charlie', 'delta', 'echo']
+word_list = [('alpha', 'letter_a'), ('charlie', 'letter_c')]
 
 def test_peek():
-	assert_equal(parser.peek(word_list), 'a')
+	assert_equal(parser.peek(word_list), 'alpha')
 	result = parser.peek(word_list)
-	assert_equal(result, 'a')
+	assert_equal(result, 'alpha')
 	
 
 	
