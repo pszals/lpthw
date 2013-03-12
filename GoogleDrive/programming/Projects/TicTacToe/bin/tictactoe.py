2de8447efcf0f6551217249aba0a42fecbeb12d7
Philip Szalwinski <pszalwinski@gmail.com>
2013-03-12 18:26:34 -0400

    Added function to place O's on the board.

--- GoogleDrive/programming/Projects/TicTacToe/bin/tictactoe.py
+++ GoogleDrive/programming/Projects/TicTacToe/bin/tictactoe.py
@@ -10,16 +10,29 @@ def print_board(board):
 print_board(board) 
 
 def place_x():
+	print "It's X's turn."
 	row = int(raw_input("Row: ")) - 1
 	col = int(raw_input("Column: ")) - 1
 	
-	
-	
 	if (board[row][col] != "X") and (board[row][col] != "O"):
 		board[row][col] = "X"
+	elif (1 > row > 3):
+		print "That is not on the board."
 	else:
-		print "Something is wrong"
+		print "That is not a legal move. Pick a spot where there is no X or O."
 	print_board(board)
 
-place_x()
+def place_o():
+	print "It's O's turn."
+	row = int(raw_input("Row: ")) - 1
+	col = int(raw_input("Column: ")) - 1
+	
+	if (board[row][col] != "X") and (board[row][col] != "O"):
+		board[row][col] = "O"
+	elif (1 > row > 3):
+		print "That is not on the board."
+	else:
+		print "That is not a legal move. Pick a spot where there is no X or O."
+	print_board(board)
 
+place_o()
